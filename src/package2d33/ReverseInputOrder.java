1f760c627b448b5c029865a2f2950bae473197b7
Abhinand Shibu <abhinandshibu@outlook.com>
2021-01-22 11:49:48 +0000

    Question 2d33 Done

--- src/package2d33/ReverseInputOrder.java
+++ src/package2d33/ReverseInputOrder.java
@@ -1,4 +1,27 @@
 package package2d33;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class ReverseInputOrder {
+
+    public static void main(String[] args) throws IOException {
+        final Deque<String> inputs = new ArrayDeque<>();
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // tried line != null, but i don't know when it becomes null, it just carried
+        // on forever
+        String line = br.readLine();
+        while (!line.isEmpty()) {
+            inputs.push(line);
+            line = br.readLine();
+        }
+
+        while (!inputs.isEmpty()) {
+            System.out.println(inputs.pop());
+        }
+    }
 }
